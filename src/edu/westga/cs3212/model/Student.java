e6699823a94339dc73776a54b64b174db85d3ad9
Javon Onuigbo <jonuigb1@my.westga.edu>
2018-03-12 14:33:21 -0400

    Created class Student

---
+++ src/edu/westga/cs3212/model/Student.java
@@ -0,0 +1,16 @@
+package edu.westga.cs3212.model;
+
+public class Student {
+	private int grade;
+	
+	
+	public int getGrade() {
+		return this.grade;
+	}
+	
+	public void setGrade(int grade) {
+		this.grade = grade;
+		
+	}
+
+}
