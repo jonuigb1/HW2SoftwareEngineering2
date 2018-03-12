c27f858491b6b6d51c6b9688434d1bbd0921bdd1
Javon Onuigbo <jonuigb1@my.westga.edu>
2018-03-12 17:39:28 -0400

    added the getLetterGrade method

--- src/edu/westga/cs3212/model/PrintGrade.java
+++ src/edu/westga/cs3212/model/PrintGrade.java
@@ -4,6 +4,26 @@ import java.util.ArrayList;
 
 public class PrintGrade {
 	
+	public String getLetterGrade(int grade) {
+		String letterGrade = "";
+		if(grade >=90) {
+			letterGrade = "A";
+		}
+		else if( grade >= 80) {
+			letterGrade = "B";
+		}
+		else if(grade >= 70) {
+			letterGrade ="C";
+		}
+		else if(grade >=60) {
+			letterGrade ="D";
+		}
+		else {
+			letterGrade ="F";
+		}
+		return letterGrade;
+	}
+	
 	
 	public String getPlusOrMinus(int grade) {
 		String finalGrade = " ";
